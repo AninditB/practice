44fb28a2f63ff1120d29061855d02aa721bc1e20
yyi10 <aninditbordoloi@gmail.com>
2025-05-20 22:52:33 -0500

    Group Anagram

---
+++ java/49.GroupAnagram/groupAnagram_sorted.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class groupAnagram_sorted {
+    public LIst<List<String>> groupAnagram(String[] strs) {
+        if (strs.length == 0)
+            return new ArrayList<>();
+        Map<String, List<String>> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String key = String.valueOf(charArray);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+        return new ArrayList<>(ans.values());
+    }
+}
