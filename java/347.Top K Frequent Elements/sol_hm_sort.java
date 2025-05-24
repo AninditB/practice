758ddcae8faa1645df0abcf76c849a1fe736cada
yyi10 <aninditbordoloi@gmail.com>
2025-05-23 23:39:37 -0500

    correction +1

--- java/347.Top K Frequent Elements/sol_hm_sort.java
+++ java/347.Top K Frequent Elements/sol_hm_sort.java
@@ -5,12 +5,15 @@ public class sol_hm_sort {
         for (int num : nums) {
             freqMap.put(nu, freqMap.getOrDefault(num, 0) + 1);
         }
+
         List<Map.Entry<Ineger, Integer>> freqList = new ArrayList<>(freqMap.entrySet());
         frequList.sort((a, b) -> b.getValue() - a.getValue());
+
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
             result[i] = freqList.get(i).getKey();
         }
+
         return result;
     }
 }
