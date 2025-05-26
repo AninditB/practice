d95cdac130b53a6675cdd10860c2af2debe97186
yyi10 <aninditbordoloi@gmail.com>
2025-05-25 22:58:21 -0500

    update + 1

--- java/347.Top K Frequent Elements/sol_hm_sort.java
+++ java/347.Top K Frequent Elements/sol_hm_sort.java
@@ -1,13 +1,14 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class sol_hm_sort {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
         for (int num : nums) {
-            freqMap.put(nu, freqMap.getOrDefault(num, 0) + 1);
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        List<Map.Entry<Ineger, Integer>> freqList = new ArrayList<>(freqMap.entrySet());
-        frequList.sort((a, b) -> b.getValue() - a.getValue());
+        List<Map.Entry<Integer, Integer>> freqList = new ArrayList<>(freqMap.entrySet());
+        freqList.sort((a, b) -> b.getValue() - a.getValue());
 
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
