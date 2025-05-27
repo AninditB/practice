3e9e090261858e6a0c23c4d353ee34927c660450
yyi10 <aninditbordoloi@gmail.com>
2025-05-26 21:56:54 -0500

    update +1

--- java/347.Top K Frequent Elements/sol_hm_sort.java
+++ java/347.Top K Frequent Elements/sol_hm_sort.java
@@ -13,17 +13,20 @@ public class sol_hm_sort {
         for (int num : nums) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
+
         // Step 2: Convert freqMap to list of (num, frequency) pairs
         // Create list freqList from entries of freqMap
         List<Map.Entry<Integer, Integer>> freqList = new ArrayList<>(freqMap.entrySet());
+
         // Step 3: Sort freqList by descending frequency
         // Sort freqList based on value in descending order
         freqList.sort((a, b) -> b.getValue() - a.getValue());
+
         // Step 4: Get top k keys
         // Create empty list result of size k
-        // For i from 0 to k - 1:
-        // result[i] = freqList[i].key
         int[] result = new int[k];
+        // For i from 0 to k - 1:
+        // --- result[i] = freqList[i].key
         for (int i = 0; i < k; i++) {
             result[i] = freqList.get(i).getKey();
         }
